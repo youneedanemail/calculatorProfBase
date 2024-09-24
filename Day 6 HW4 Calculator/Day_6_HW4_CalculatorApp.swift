294e957fcd7730faf1a0abfb7b2cb16b269bcc19
Steve Liddle <liddle@byu.edu>
2024-09-24 13:10:00 -0600

    Initial Commit

---
+++ Day 6 HW4 Calculator/Day_6_HW4_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_6_HW4_CalculatorApp.swift
+//  Day 6 HW4 Calculator
+//
+//  Created by Stephen Liddle on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_6_HW4_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

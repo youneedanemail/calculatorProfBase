e22e31f1707ee5d6c092276d1422bb3107b232c4
Steve Liddle <liddle@byu.edu>
2024-09-27 08:05:32 -0600

    Tightened visibility, dropped commented-out code.

--- Day 6 HW4 Calculator/ViewModel/CalculatorBrain.swift
+++ Day 6 HW4 Calculator/ViewModel/CalculatorBrain.swift
@@ -11,18 +11,13 @@ import Foundation
 
     // MARK: - Properties
 
-    var soundPlayer = SoundPlayer()
+    private var soundPlayer = SoundPlayer()
 
     // MARK: - Model access
 
     // MARK: - User intents
 
     func clickButton() {
-//        let path = Bundle.main.path(forResource: "Click", ofType: "m4a")
-//
-//        if let path {
-//            let url = URL(fileURLWithPath: path)
-//        }
         soundPlayer.playSound(named: "Click.m4a")
     }
 
